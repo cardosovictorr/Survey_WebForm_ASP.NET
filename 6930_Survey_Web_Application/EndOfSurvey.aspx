9493c59b9dc86c0d0a92c2f7714a2989fd9abb76
cardosovictorr <62979369+cardosovictorr@users.noreply.github.com>
2021-04-11 22:24:43 +1000

    organizibg the questions and fixing few errors

---
+++ 6930_Survey_Web_Application/EndOfSurvey.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="EndOfSurvey.aspx.cs" Inherits="_6930_Survey_Web_Application.EndOfSurvey" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
---
+++ 6930_Survey_Web_Application/Model/QuestionAnswers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _6930_Survey_Web_Application.Model
+{
+    public class QuestionAnswers
+    {
+        private int id;
+        private string option_text;
+        private int q_id;
+        private int? option_id;
+
+        public int Id { get => id; set => id = value; }
+        public string Option_text { get => option_text; set => option_text = value; }
+        public int Q_id { get => q_id; set => q_id = value; }
+        public int? Option_id { get => option_id; set => option_id = value; }
+    }
+}
